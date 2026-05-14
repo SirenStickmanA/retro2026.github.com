c7e008adb359ac99039c8f2a2fff86c3a13c5dd8
SirenStickmanA <ribeirojesusc@gmail.com>
2026-05-14 11:02:39 -0300

    Add files via upload

---
+++ includes.php
@@ -0,0 +1,9 @@
+<?php
+  session_start();
+  include "maintenance.php";
+  include "database.php";
+?>
+
+
+<!-- By Ficello.zip#9910 if you want to see the source code: https://github.com/Ficelloo/2006-Roblox-Website --> 
+<!-- would be cool if ur not removing this credit lol -->
